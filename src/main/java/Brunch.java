67536d8a822b8476fc171d28abb0ebe099b42794
Alberto Pose <albertopose@gmail.com>
2012-12-14 20:24:09 -0300

    WIP

--- src/main/java/Brunch.java
+++ src/main/java/Brunch.java
@@ -1,17 +1,20 @@
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Function;
+import org.mozilla.javascript.*;
 import org.mozilla.javascript.tools.shell.Global;
 import org.mule.tools.rhinodo.api.NodeModule;
 import org.mule.tools.rhinodo.api.Runnable;
 import org.mule.tools.rhinodo.impl.JavascriptRunner;
 import org.mule.tools.rhinodo.impl.NodeModuleFactoryImpl;
 import org.mule.tools.rhinodo.impl.NodeModuleImpl;
+import org.mule.tools.rhinodo.impl.NodeModuleImplBuilder;
 import org.mule.tools.rhinodo.rhino.RhinoHelper;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Brunch implements Runnable {
     public static final String RHINODO_FOLDER = ".rhinodo";
@@ -19,8 +22,10 @@ public class Brunch implements Runnable {
     private Global global;
     private final RhinoHelper rhinoHelper;
     private final JavascriptRunner javascriptRunner;
+    private final NodeModuleImpl mime;
 
-    public Brunch(){
+    public Brunch() {
+
         this.rhinoHelper = new RhinoHelper();
         String userHome = System.getProperty("user.home");
 
@@ -28,48 +33,51 @@ public class Brunch implements Runnable {
         String destDirString = destDir.toString();
         destDir.mkdirs();
 
-        NodeModule brunch = NodeModuleImpl.fromJar(this.getClass(), "META-INF/brunch", destDirString);
+        NodeModule brunch = NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/brunch", destDirString);
+        mime = NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/mime", destDirString);
 
         List<? extends NodeModule> nodeModuleList = Arrays.asList(
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/ansi-color", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/argumentum", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/async", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/brunch", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/bytes", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/chokidar", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/coffee-script", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/commander", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/connect", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/cookie", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/crc", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/date-utils", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/debug", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/diff", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/express", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/fast-list", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/forEachAsync", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/formidable", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/fresh", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/graceful-fs", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/growl", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/handlebars", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/inflection", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/jade", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/methods", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/mime", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/mkdirp", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/mocha", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/ncp", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/optimist", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/pause", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/qs", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/range-parser", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/rimraf", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/send", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/sequence", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/uglify-js", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/walk", destDirString),
-                NodeModuleImpl.fromJar(this.getClass(), "META-INF/wordwrap", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/ansi-color", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/argumentum", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/async", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/brunch", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/bytes", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/chokidar", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/clean-css", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/coffee-script", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/commander", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/connect", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/cookie", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/crc", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/date-utils", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/debug", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/diff", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/express", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/fast-list", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/forEachAsync", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/formidable", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/fresh", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/graceful-fs", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/growl", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/handlebars", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/inflection", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/jade", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/methods", destDirString),
+                mime,
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/mkdirp", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/mocha", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/less", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/ncp", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/optimist", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/pause", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/qs", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/range-parser", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/rimraf", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/send", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/sequence", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/uglify-js", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/walk", destDirString),
+                NodeModuleImplBuilder.fromJarOrFile(this.getClass(), "META-INF/node_modules/wordwrap", destDirString),
                 brunch);
 
         javascriptRunner = new JavascriptRunner(new NodeModuleFactoryImpl(nodeModuleList),
@@ -84,13 +92,25 @@ public class Brunch implements Runnable {
     public void executeJavascript(final Context context, final Global global) {
         this.ctx = context;
         this.global = global;
+
         Function require = (Function)global.get("require", global);
-        Object result = require.call(ctx,global,global,new String [] {"brunch"});
+        Scriptable result = (Scriptable) require.call(ctx, global, global, new String[]{"brunch"});
+        BoundFunction build = (BoundFunction) Context.jsToJava(result.get("build", result), Function.class);
+        Map<String, String> options = new HashMap<String,String>();
+        options.put("mimify", "true");
+        System.setProperty("user.dir","/Users/apose/Documents/habitat/web-ui/habitat-ui-ember");
+        Object object = ScriptRuntime.doTopCall(build, ctx, global, global,
+                new Object[]{ new RhinoHelper().mapToNativeObject(options), new BaseFunction(){
+                    @Override
+                    public Object call(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+//                        System.out.println(args);
+                        return Undefined.instance;
+                    }
 
-//        var src = path.join(path.dirname(fs.realpathSync(__filename)), '..', 'lib');
-//        require(path.join(src, 'cli')).run();
-
-
-
+                    @Override
+                    public Object getDefaultValue(Class<?> typeHint) {
+                        return "Callback function of brunch build";
+                    }
+                }});
     }
 }
